cc263b521e4dc83af0f54ce19413dfb366ea6ea9
hackpres <102390515+hackpres@users.noreply.github.com>
2022-09-02 18:07:14 -0400

    organized my controller layout

---
+++ controllers/thoughtController.js
@@ -0,0 +1,20 @@
+const { Thoughts } = require('../models');
+
+const thoughtController = {
+    //create new thought
+
+    //get all thoughts
+
+    //get thought by ID
+
+    //update thought by ID
+
+    //delete thought by ID
+
+    //create new reaction
+
+    //delete reaction by ID
+
+}
+
+module.exports = thoughtController;
---
+++ controllers/userController.js
@@ -0,0 +1,17 @@
+const { Users } = require('../models');
+
+const userController = {
+    //create new user
+
+    //get all users
+
+    //get user by ID
+
+    //update user by ID
+
+    //delete user by ID
+
+    //delete a friend
+}
+
+module.exports = userController;
